1e85ed7f069719aad48d8ee155161bd9e0b4c065
shooga12 <shooge.alotaibi@gmail.com>
2022-09-08 01:28:13 +0300

    List Of Stores Feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'screens/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: MyApp(),
+  ));
 }
+
+// child: ElevatedButton(
+//   onPressed: () {
+//     Navigator.pop(context);
+//   },
+//   child: const Text('Go back!'),
+// ),
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -17,32 +29,58 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
-
-  void signup() async {
-    try {
-      await firebaseAuth.createUserWithEmailAndPassword(
-          email: "Noura@gmail.com", password: "123456");
-    } catch (e) {
-      print(e);
-    }
-  }
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomePage(),
+  ];
+  // void signup() async {
+  //   try {
+  //     await firebaseAuth.createUserWithEmailAndPassword(
+  //         email: "shooge@gmail.com", password: "12345");
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("firebase"),
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/background.png"),
+                  fit: BoxFit.fill)),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              signup();
-            },
-            child: Text("signup"),
-          ),
-        ),
+        toolbarHeight: 170,
+        //leading: BackButton(),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
+      body: _children[_currentIndex],
     );
+    //   return MaterialApp(
+    //     home: Scaffold(
+    //       extendBodyBehindAppBar: true,
+    //       appBar: AppBar(
+    //         flexibleSpace: Container(
+    //           decoration: BoxDecoration(
+    //               image: DecorationImage(
+    //                   image: AssetImage("assets/background.png"),
+    //                   fit: BoxFit.fill)),
+    //         ),
+    //         toolbarHeight: 170,
+    //         //leading: BackButton(),
+    //         backgroundColor: Colors.transparent,
+    //         elevation: 0,
+    //       ),
+    //       body: Center(
+    //         child: ElevatedButton(
+    //             child: const Text('Open route'),
+    //             onPressed: () {
+    //             }),
+    //       ),
+    //     ),
+    //   );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:taqdaa_application/main.dart';
+import '/Users/shougalotaibi/Taqdaa/lib/screens/home_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
