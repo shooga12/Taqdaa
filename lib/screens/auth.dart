940d1e0ee3783b11477e1bc843cd39603777351a
Nourafk <99095586+Nourafk@users.noreply.github.com>
2022-09-07 16:32:44 +0300

    Uploading project folder

---
+++ lib/screens/auth.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User? user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
