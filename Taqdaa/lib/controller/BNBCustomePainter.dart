d6c0fb6e2fcd2b9d42395b5c9477995974cfa1ce
shhuudu <shahaddesu@gmail.com>
2022-10-09 10:09:09 +0300

    view return requests
    view return requests

---
+++ Taqdaa/lib/controller/BNBCustomePainter.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BNBCustomePainter extends CustomPainter {
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Color.fromARGB(255, 32, 7, 121)
+      ..style = PaintingStyle.fill;
+    Path path = Path()..moveTo(0, 20);
+    path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
+    path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
+    path.arcToPoint(Offset(size.width * 0.60, 20),
+        radius: Radius.circular(10.0), clockwise: false);
+    path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
+    path.quadraticBezierTo(size.width * 0.80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawShadow(path, Colors.black, 5, true);
+    canvas.drawPath(path, paint);
+  }
+
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
