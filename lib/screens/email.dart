a3adc096c7f5374b226a258e38b942d2143ae337
Alanoudmoh3 <98808440+Alanoudmoh3@users.noreply.github.com>
2022-10-23 10:48:03 +0300

    email

---
+++ lib/screens/email.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import '../main.dart';
+
+class email extends StatefulWidget {
+  const email({super.key});
+
+  @override
+  State<email> createState() => _emailState();
+}
+
+class _emailState extends State<email> {}
