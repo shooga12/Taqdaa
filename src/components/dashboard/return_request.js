ad527dff92f860f6a3cb6228bf12e5b72562895b
Reem <reem66am@gmail.com>
2022-10-23 12:37:59 +0300

    Return Requests

--- src/components/dashboard/return_request.js
+++ src/components/dashboard/return_request.js
@@ -1,15 +1,58 @@
 import React from 'react';
 
+function RequestCard({ invoices }) {
+
+    return(
+        <>
+        </>
+    )
+}
 function Return_Req(){
+
+    const [done,setDone] = useState(0);
+    const [requests,setRequests] = useState();
+
+    useEffect(() => {
+    getData();
+    console.log("Data:");
+    },[done]);
+    
+    //Retrieve Return Requests
+    const getData = async ()=>{
+        let storeName;
+        //Get Store Name
+        let docRef = doc(db, "Stores", 'Store'+auth.currentUser?.uid);
+        let docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            storeName = docSnap.data().StoreName;
+        } 
+        else {
+          console.log("No such document!");
+        }
+
+        //Get Return Requests of Store          
+        docRef = collection(db, "RequestsManager"+storeName);
+        getDocs(docRef).then((querySnapshot) => {
+          const tempArr = []
+          querySnapshot.forEach((doc) => {
+            tempArr.push(doc.data());
+          }) 
+          setRequests(tempArr); 
+        })
+    }
+
+
+
     const handleFile = (e) => {
        
     }
-        return(  
-            <>
-            <h1 class="mt-3">Return Requests</h1>
-           
-            </>     
-        )
+
+    return(  
+        <>
+        <h1 class="mt-3">Return Requests</h1>
+        
+        </>     
+    )
     
 }
 export default Return_Req;
